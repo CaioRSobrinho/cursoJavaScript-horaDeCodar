395caeab9d641fa5df5d388fe62b4cf461b09d33
Caio Ruan <168470719+CaioRSobrinho@users.noreply.github.com>
2025-01-14 21:43:01 -0300

    Adicionando exercícios de JS

---
+++ 3_particularidades_js/js/script.js
@@ -0,0 +1,18 @@
+console.log('primeiro');
+
+console.log('ultimo');
+
+console.log('mais um console.log');
+
+var a = 1;
+a = 'teste';
+
+console.log(a);
+
+//este é um comentario
+
+/*
+comentarios
+de multiplas
+linhas
+*/
---
+++ 4_variaveis/js/script.js
@@ -0,0 +1,39 @@
+var teste = 1;
+
+nome = 'caio'
+
+console.log(teste)
+
+teste = 'caio'
+
+console.log(teste)
+
+console.log(nome)
+
+var $nome = 'abc'
+var _nome = 'abc'
+
+console.log($nome)
+console.log(_nome)
+
+var meuPrimeiroNome = 'Caio' //camelCase
+
+console.log(meuPrimeiroNome)
+
+var meusobrenome = 'Ruan'
+
+console.log(meusobrenome)
+
+let testando = 1
+const ola = 2
+
+console.log(testando)
+console.log(ola)
+
+var meuNome
+
+console.log(meuNome)
+
+meuNome = 'Caio'
+
+console.log(meuNome)
