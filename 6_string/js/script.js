8340fcbc8dc9d0f70c3ed96bd7caeb26f6d8efe2
Caio Ruan <168470719+CaioRSobrinho@users.noreply.github.com>
2025-01-16 21:50:11 -0300

    adicionando exercícios de javascript

---
+++ 6_string/js/script.js
@@ -0,0 +1,26 @@
+var nome ="caio"
+var sobrenome = "ruan"
+
+var nomeCompleto = nome + " " + sobrenome
+
+console.log(nome)
+console.log(typeof nome)
+
+console.log(typeof "asd")
+
+console.log(nomeCompleto)
+
+console.log(typeof '5.292929')
+
+var frase = "está é uma frase"
+
+console.log(frase)
+console.log(typeof frase)
+
+console.log(nome + "" + frase)
+
+document.write('Ele disse "olá"')
+
+document.write("Ele disse 'ola'")
+
+console.log("Este número:" + nome)
---
+++ 8_undefined_null/js/script.js
@@ -0,0 +1,15 @@
+var obj = {
+    nome: 'caio',
+    idade: 27,
+    profissao: 'Desenvolvedor',
+    estaTrabalhando: true,
+};
+
+console.log(obj)
+console.log(typeof obj)
+
+console.log(obj.nome)
+console.log(obj.idade)
+console.log(obj.profissao)
+
+console.log("O meu nome é" + obj.nome)
